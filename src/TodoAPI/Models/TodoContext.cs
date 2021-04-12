8f89b07387b4e82fadab806eb895746d6db35fdb
varunmaurya37 <varunmaurya37@gmail.com>
2021-04-12 09:16:16 +0530

    Added DbContext for Todo Model

---
+++ src/TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAPI.Models
+{
+    public class TodoContext: DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options): base(options)
+        {
+        }
+
+        public DbSet<Todo> TodoItems {get; set;}
+
+    }
+}
