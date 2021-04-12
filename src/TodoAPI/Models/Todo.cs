82c80a705b0925c8562964fe03b987ebdee20f6d
varunmaurya37 <varunmaurya37@gmail.com>
2021-04-12 08:56:21 +0530

    Added Todo MOdel to API Project

---
+++ src/TodoAPI/Models/Todo.cs
@@ -0,0 +1,11 @@
+namespace TodoAPI.Models
+{
+    public class Todo
+    {
+        public int Id {get; set;}
+        public string Title {get; set;}
+        public string Description {get; set;}
+
+        //! TODO - Add CreateTime property
+    }
+}
