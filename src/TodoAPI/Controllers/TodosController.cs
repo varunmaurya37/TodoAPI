47b8eedb37124befd806549804a1c14ff281a7c2
varunmaurya37 <varunmaurya37@gmail.com>
2021-04-13 18:25:00 +0530

    Added POST, PUT, DELETE Methods

--- src/TodoAPI/Controllers/TodosController.cs
+++ src/TodoAPI/Controllers/TodosController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoAPI.Models;
 
 namespace TodoAPI.Controllers 
@@ -16,6 +17,55 @@ namespace TodoAPI.Controllers
         public ActionResult<IEnumerable<Todo>> GetTodos() 
         {
             return Ok(_context.TodoItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Todo> GetTodoItem(int id) {
+            var todoItem = _context.TodoItems.Find(id);
+
+            if(todoItem == null) 
+            {
+                return NotFound();
+            }
+            return Ok(todoItem);
+        }
+
+        [HttpPost]
+        public ActionResult<Todo> CreateTodo(Todo todo) 
+        {
+            _context.TodoItems.Add(todo);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetTodoItem", new Todo{Id = todo.Id}, todo);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateTodoItem(int id, Todo todo) 
+        {
+            if(id != todo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todo).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Todo> DeleteTodo(int id) 
+        {
+            var todoItem = _context.TodoItems.Find(id);
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+            _context.TodoItems.Remove(todoItem);
+            _context.SaveChanges();
+
+            return todoItem;
         }
     }
 }
